e45a03e396fe93ef50a396605e919c58b0dd01d2
Zhen Wei Liao <zhenwei.liao@stonybrook.edu>
2024-02-12 22:32:41 -0500

    Installed React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./stylesheets/App.scss"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={null} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
