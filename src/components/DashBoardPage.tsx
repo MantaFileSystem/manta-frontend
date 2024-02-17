7ace299f91afe4a50cf20e655f754d947d2e53e8
Zhen Wei Liao <zhenwei.liao@stonybrook.edu>
2024-02-16 23:07:52 -0500

    Changed div to motion.div

--- src/components/DashBoardPage.tsx
+++ src/components/DashBoardPage.tsx
@@ -1,13 +1,15 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import { routeVariants } from "../helper/RouterAnimation";
 
 export default function DashBoardPage() {
-    return (
-        <motion.div 
-            className="content"
-            initial="initial"
-            animate="final"
-            variants={routeVariants}
-        >
-            DashBoardPage</motion.div>);
+  return (
+    <motion.div
+      className="content"
+      initial="initial"
+      animate="final"
+      variants={routeVariants}
+    >
+      DashBoardPage
+    </motion.div>
+  );
 }
--- src/components/PeerPage.tsx
+++ src/components/PeerPage.tsx
@@ -1,3 +1,15 @@
+import { motion } from "framer-motion";
+import { routeVariants } from "../helper/RouterAnimation";
+
 export default function PeerPage() {
-  return <div >PeerPage</div>;
+  return (
+    <motion.div
+      className="content"
+      initial="initial"
+      animate="final"
+      variants={routeVariants}
+    >
+      PeerPage
+    </motion.div>
+  );
 }
