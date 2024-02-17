0a182f83085d0fd639e3350b8c80c03491e7b1f8
Zhen Wei Liao <zhenwei.liao@stonybrook.edu>
2024-02-16 19:08:02 -0500

    Finish implementing the side bar menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,59 @@
+import Logo from "../svg/Logo.tsx";
+import { NavLink } from "react-router-dom";
+import {
+  PersonGear,
+  CloudArrowUp,
+  DatabaseAdd,
+  Gear,
+  InfoCircle,
+} from "react-bootstrap-icons";
+
+const iconColor: string = "white",
+  iconSize: number = 38;
+
+function Tabs() {
+  return (
+    <div className="tabs">
+      <NavLink to="/">
+        <div>
+          <PersonGear color={iconColor} size={iconSize} />
+        </div>
+      </NavLink>
+      <NavLink to="/file-upload">
+        <div>
+          <CloudArrowUp color={iconColor} size={iconSize} />
+        </div>
+      </NavLink>
+      <NavLink to="/peer">
+        <div>
+          <DatabaseAdd color={iconColor} size={iconSize} />
+        </div>
+      </NavLink>
+      <NavLink to="/setting">
+        <div>
+          <Gear color={iconColor} size={iconSize} />
+        </div>
+      </NavLink>
+    </div>
+  );
+}
+
+function Support() {
+  return (
+    <div className="support">
+      <div>
+        <InfoCircle color={iconColor} size={iconSize - 5} />
+      </div>
+    </div>
+  );
+}
+
+export default function Menu() {
+  return (
+    <div className="menu">
+      <Logo fill="white" />
+      <Tabs />
+      <Support />
+    </div>
+  );
+}
