0169259fdcc74d744e45696422bca7b0dd9529e6
Zhen Wei Liao <zhenwei.liao@stonybrook.edu>
2024-02-26 14:40:41 -0500

    Removed OrcaConnection placeholder text

--- src/components/Setting/OrcaCoinConnection.tsx
+++ src/components/Setting/OrcaCoinConnection.tsx
@@ -1,7 +1,7 @@
 export default function OrcaCoinConnection() {
   return (
     <div className="orca-coin-connection">
-      <h1 className="text-3xl font-bold underline">OrcaCoin Connected</h1>
+      <h1 className="text-3xl font-bold underline"></h1>
     </div>
   );
 }
